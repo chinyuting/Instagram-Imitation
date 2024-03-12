dcf3ab0f5a07f2ed6c78dd2d161ef83ebb8e6c0e
kkmao <bra224max852zxc@gmail.com>
2024-03-12 22:12:23 +0800

    say hello world

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+@import "./assets/all.scss";
+</style>
+<!--hello world-->
